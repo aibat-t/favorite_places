8a84625526ec3298b9f61da8b77338b2e1a8942b
Tozhybayev Aibat <a.tozhybayev@alantech.kz>
2024-05-09 18:00:10 +0500

    location add func

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -80,6 +81,8 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
                   _selectedFile = image;
                 },
               ),
+              const SizedBox(height: 10),
+              LocationInput(),
               const SizedBox(height: 16),
               ElevatedButton.icon(
                 onPressed: _isSending ? null : _savePlace,
